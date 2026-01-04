2c3021c6592b13f09ed50c63ff64676845bc26d3
track-prepped-68-Corolla <jsetne@pm.me>
2026-01-04 13:15:21 -0500

    tried removing display maneger from sddm for gamescope compatibility

--- modules/services/sddm.nix
+++ modules/services/sddm.nix
@@ -14,10 +14,10 @@
 
     services.xserver.enable = lib.mkDefault true;
 
-    services.displayManager.sddm = {
-      enable = lib.mkDefault true;
-      wayland.enable = lib.mkDefault true;
-    };
+  #  services.displayManager.sddm = {
+   #   enable = lib.mkDefault true;
+   #   wayland.enable = lib.mkDefault true;
+  #  };
 
     services.xserver.xkb = {
       layout = lib.mkDefault "us";
