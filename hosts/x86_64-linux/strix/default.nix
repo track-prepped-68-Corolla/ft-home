a7ed372611c988771b2e211db094cb0080bafa6b
68Corolla <184780572+track-prepped-68-Corolla@users.noreply.github.com>
2026-05-09 22:51:01 -0400

    feat(strix): enable CachyOS kernel

--- hosts/x86_64-linux/strix/default.nix
+++ hosts/x86_64-linux/strix/default.nix
@@ -2,27 +2,15 @@
 
 {
   imports = [
-    # 1. The hardware scan for this specific machine
     ./hardware-configuration.nix
-
-    # 2. The Shared Module Library (Magic Collator)
     ../../../modules/nixos
   ];
 
   networking.hostName = "strix";
 
-  # Now the module handles 'joe' as an admin automatically
   mainuser = "joe";
-
   superUsers = [ "joe" ];
 
-  # Add Joe's specific Yubikey mapping here
-  #u2fMappings = ''
-  #  joe:your_yubikey_public_key_string_here
-  #'';
-
-  # Define the password for your main user.
-  # 'initialPassword' allows you to change it later; 'password' would enforce it.
   users.users.joe.initialPassword = "nixos";
   users.mutableUsers = true;
 
@@ -32,6 +20,7 @@
   ft.security.sops.useTPM = true;
 
   ft.desktop.cosmic.enable = true;
+  ft.kernel.cachyos.enable = true;
 
   ft.cli.enable = true;
 
