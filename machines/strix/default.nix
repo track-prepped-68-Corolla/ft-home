d6e6008c682c8c12fe5de66019d1f3674157d0da
track-prepped-68-Corolla <184780572+track-prepped-68-Corolla@users.noreply.github.com>
2026-06-14 22:34:13 -0400

    ft switch is back
    Generation: 314
    
    <<< /nix/var/nix/profiles/system-313-link
    >>> /nix/var/nix/profiles/system-314-link
    No version or selection state changes.
    Closure size: 2368 -> 2368 (14 paths added, 14 paths removed, delta +0, disk usage +840B).

--- machines/strix/default.nix
+++ machines/strix/default.nix
@@ -52,7 +52,7 @@
   ft.dockervm.sshAuthorizedKeys = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDgZCe1UZA1E7bCpTWz5NUMHlGUq16nOobSJ2LyyZCP2AAAABHNzaDo= track-prepped-68-Corolla@protonmail.com"
   ];
-  
+
   ft.hermesVm = {
     enable = true;
     vmName = "hermes";
@@ -61,8 +61,6 @@
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDgZCe1UZA1E7bCpTWz5NUMHlGUq16nOobSJ2LyyZCP2AAAABHNzaDo= track-prepped-68-Corolla@protonmail.com"
     ];
   };
-
-
 
   ft.ssh = {
     enable = true;
